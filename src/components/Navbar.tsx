0e3a8a8fc8bd3799272352eba44520a711b6f516
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-19 00:25:24 +0600

    create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className="flex bg-green-800 h-16 text-white">
+            <h2 className="text-2xl mt-4 ml-36 font-bold">Static Pages</h2>
+        </div>
+    );
+};
+
+export default Navbar;

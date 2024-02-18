a79245e2e4ed2bb9def97b5a28e0dbeb84bc724e
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-19 00:26:23 +0600

    update navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="flex bg-green-800 h-16 text-white">
             <h2 className="text-2xl mt-4 ml-36 font-bold">Static Pages</h2>
+            <div className="flex text-end ml-64">
+                <NavLink className="text-1xl ml-36 mt-5" to="/" >Home </NavLink>
+                <div className="ml-12 mt-5 text-black">|</div>
+                <NavLink className="text-1xl ml-10 mt-5" to="/About" >About </NavLink>
+                <div className="ml-12 mt-5 text-black">|</div>
+                <NavLink className="text-1xl ml-10 mt-5" to="/Contacts" >Contacts </NavLink>
+                <div className="ml-12 mt-5 text-black">|</div>
+                <NavLink className="text-1xl ml-10 mt-5" to="/Divisions" >Divisions </NavLink>
+                <div className="ml-12 mt-5 text-black">|</div>
+                <NavLink className="text-1xl ml-10 mt-5 text-black font-bold" to="/Admin" >Admin</NavLink>
+            </div>
         </div>
     );
 };
